9ebb9c163dd00f172332efae1e3894095c33737c
bkmartinjr <bruce@chanzuckerberg.com>
2022-12-05 17:16:28 +0000

    improve docstrings

--- api/python/cell_census/src/cell_census/release_directory.py
+++ api/python/cell_census/src/cell_census/release_directory.py
@@ -19,8 +19,8 @@ CensusReleaseDescription = TypedDict(
     {
         "release_date": Optional[str],  # date of release, optional
         "release_build": str,  # date of build
-        "soma": CensusLocator,
-        "h5ads": CensusLocator,
+        "soma": CensusLocator,  # SOMA objects locator
+        "h5ads": CensusLocator,  # source H5ADs locator
     },
 )
 CensusDirectory = Dict[CensusReleaseTag, Union[CensusReleaseTag, CensusReleaseDescription]]
@@ -31,7 +31,34 @@ CELL_CENSUS_RELEASE_DIRECTORY_URL = "https://s3.us-west-2.amazonaws.com/cellxgen
 
 
 def get_release_description(tag: str) -> CensusReleaseDescription:
-    """Get release description for given tag. Raises KeyError if unknown tag value."""
+    """
+    Get release description for given tag from the Cell Census release directory.
+    Raises KeyError if unknown tag value.
+
+    Parameters
+    ----------
+    tag : str
+        The release tag or name.
+
+    Returns
+    -------
+    CensusReleaseDescription
+        Dictionary containing a description of the release.
+
+    See Also
+    --------
+    get_directory : returns the entire directory as a dict.
+
+    Examples
+    --------
+    >>> cell_census.get_release_description("latest")
+    {'release_date': None,
+    'release_build': '2022-12-01',
+    'soma': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/soma/',
+    's3_region': 'us-west-2'},
+    'h5ads': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/h5ads/',
+    's3_region': 'us-west-2'}}
+    """
     census_directory = get_directory()
     description = census_directory.get(tag, None)
     if description is None:
@@ -41,7 +68,43 @@ def get_release_description(tag: str) -> CensusReleaseDescription:
 
 def get_directory() -> Dict[CensusReleaseTag, CensusReleaseDescription]:
     """
-    Get the directory of cell census releases available.
+    Get the directory of cell census releases currently available.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    Dict[CensusReleaseTag, CensusReleaseDescription]
+        Dictionary of release tags (names) and their corresponding
+        release description.
+
+    See Also
+    --------
+    get_release_description : get release description by tag.
+
+    Examples
+    --------
+    >>> cell_census.get_directory()
+    {'latest': {'release_date': None,
+    'release_build': '2022-12-01',
+    'soma': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/soma/',
+    's3_region': 'us-west-2'},
+    'h5ads': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/h5ads/',
+    's3_region': 'us-west-2'}},
+    '2022-12-01': {'release_date': None,
+    'release_build': '2022-12-01',
+    'soma': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/soma/',
+    's3_region': 'us-west-2'},
+    'h5ads': {'uri': 's3://cellxgene-data-public/cell-census/2022-12-01/h5ads/',
+    's3_region': 'us-west-2'}},
+    '2022-11-29': {'release_date': None,
+    'release_build': '2022-11-29',
+    'soma': {'uri': 's3://cellxgene-data-public/cell-census/2022-11-29/soma/',
+    's3_region': 'us-west-2'},
+    'h5ads': {'uri': 's3://cellxgene-data-public/cell-census/2022-11-29/h5ads/',
+    's3_region': 'us-west-2'}}}
     """
     response = requests.get(CELL_CENSUS_RELEASE_DIRECTORY_URL)
     response.raise_for_status()
