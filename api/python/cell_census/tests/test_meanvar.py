0f13df4e27532b8160987ddcfea4f85fb37015ac
bkmartinjr <bruce@chanzuckerberg.com>
2022-12-12 22:27:04 +0000

    add meanvar test

---
+++ api/python/cell_census/tests/test_meanvar.py
@@ -0,0 +1,19 @@
+import numpy as np
+from cell_census.compute import OnlineMatrixMeanVariance
+from scipy import sparse
+
+
+def test_online_mean_var():
+
+    rng = np.random.default_rng()
+    matrix = sparse.random(1200, 51, density=0.1, format="coo", dtype=np.float32, random_state=rng)
+    olmv = OnlineMatrixMeanVariance(*matrix.shape)
+
+    stride = 101
+    for i in range(0, matrix.nnz, stride):
+        olmv.update(matrix.col[i : i + stride], matrix.data[i : i + stride])
+    u, var = olmv.finalize()
+
+    dense = matrix.toarray()
+    assert np.allclose(u, dense.mean(axis=0))
+    assert np.allclose(var, dense.var(axis=0), rtol=1e-3)
