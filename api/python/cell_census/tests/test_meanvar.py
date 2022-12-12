91213f91bc832c6cd6c36c80104a13dd59595161
bkmartinjr <bruce@chanzuckerberg.com>
2022-12-12 22:45:10 +0000

    typing

--- api/python/cell_census/tests/test_meanvar.py
+++ api/python/cell_census/tests/test_meanvar.py
@@ -3,7 +3,7 @@ from cell_census.compute import OnlineMatrixMeanVariance
 from scipy import sparse
 
 
-def test_online_mean_var():
+def test_online_mean_var() -> None:
 
     rng = np.random.default_rng()
     matrix = sparse.random(1200, 51, density=0.1, format="coo", dtype=np.float32, random_state=rng)
