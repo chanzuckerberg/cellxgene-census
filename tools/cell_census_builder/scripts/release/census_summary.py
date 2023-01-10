c8f852b890d954d6552f83396400fd6331cd04e8
Andrew Tolopko <atolopko-czi@users.noreply.github.com>
2023-01-10 12:00:12 -0500

    issue: 64 (#65)
    * add release summary script for release announcements
    * rename nested `tools/cell_census_builder/tools` dir to `scripts` for consistency with other script dirs

---
+++ tools/cell_census_builder/scripts/release/census_summary.py
@@ -0,0 +1,28 @@
+import sys
+import cell_census
+import pandas as pd
+
+if __name__ == '__main__':
+    census_version = sys.argv[1] if len(sys.argv) > 1 else "latest"
+
+    census = cell_census.open_soma(census_version=census_version)
+
+    COLS_TO_QUERY = [
+        ("soma_joinid", "cells"),
+        ("dataset_id", "datasets"),
+        ("cell_type_ontology_term_id", "cell types"),
+        ("tissue_ontology_term_id", "tissues"),
+        ("assay_ontology_term_id", "assays"),
+    ]
+
+    obs_df = {
+        name: experiment.obs.read_as_pandas_all(column_names=[c[0] for c in COLS_TO_QUERY])
+        for name, experiment in census["census_data"].items()
+    }
+
+    # Use Pandas to summarize and display
+    stats = [(organism, col[1], df[col[0]].nunique()) for organism, df in obs_df.items() for col in COLS_TO_QUERY]
+    print(census['census_info']['summary'].read_as_pandas_all()[['label', 'value']].to_string(index=False))
+    stats_df = pd.DataFrame(stats, columns=['organism', 'attribute', 'unique count'])
+    display_stats_df = pd.pivot(stats_df, index=['organism'], columns=['attribute'], values=['unique count'])
+    print(display_stats_df)
