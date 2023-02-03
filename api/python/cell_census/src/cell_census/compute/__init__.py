d6c673886a2651d4e943b071aabd7083b4fc0926
bkmartinjr <bruce@chanzuckerberg.com>
2023-02-03 19:58:30 +0000

    remove compute module

--- api/python/cell_census/src/cell_census/compute/__init__.py
+++
@@ -1,7 +0,0 @@
-from .highly_variable_genes import highly_variable_genes
-from .meanvar import OnlineMatrixMeanVariance
-
-__all__ = [
-    "highly_variable_genes",
-    "OnlineMatrixMeanVariance",
-]
