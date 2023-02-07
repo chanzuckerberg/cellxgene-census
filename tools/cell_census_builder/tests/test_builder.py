44519f449fdeaf0d6399a56e06b0489456637dcd
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-02-07 16:52:29 -0500

    Changed to pytest + added assertion

--- tools/cell_census_builder/tests/test_builder.py
+++ tools/cell_census_builder/tests/test_builder.py
@@ -1,6 +1,5 @@
 # General unit tests for cell_census_builder. Intention is to add more fine-grained tests for builder.
 import os
-import unittest
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
@@ -25,8 +24,8 @@ from tools.cell_census_builder.mp import process_initializer
 from tools.cell_census_builder.util import uricat
 
 
-class TestBuilder(unittest.TestCase):
-    def generate_h5ad(self):
+class TestBuilder:
+    def generate_h5ad() -> anndata.AnnData:
         X = np.random.randint(5, size=(4, 4))
         # The builder only supports sparse matrices
         X = csc_matrix(X)
@@ -84,24 +83,25 @@ class TestBuilder(unittest.TestCase):
 
         return anndata.AnnData(X=X, obs=obs, var=var, obsm=obsm, uns=uns)
 
-    def setUp(self) -> None:
-        self.td = TemporaryDirectory()
+    @classmethod
+    def setup_class(cls):
+        cls.td = TemporaryDirectory()
 
-        self.assets_path = f"{self.td.name}/h5ads"
-        os.mkdir(self.assets_path)
-        self.soma_path = f"{self.td.name}/soma"
-        os.mkdir(self.soma_path)
+        cls.assets_path = f"{cls.td.name}/h5ads"
+        os.mkdir(cls.assets_path)
+        cls.soma_path = f"{cls.td.name}/soma"
+        os.mkdir(cls.soma_path)
 
-        first_h5ad = self.generate_h5ad()
-        second_h5ad = self.generate_h5ad()
+        first_h5ad = TestBuilder.generate_h5ad()
+        second_h5ad = TestBuilder.generate_h5ad()
 
-        first_h5ad_path = f"{self.assets_path}/first.h5ad"
-        second_h5ad_path = f"{self.assets_path}/second.h5ad"
+        first_h5ad_path = f"{cls.assets_path}/first.h5ad"
+        second_h5ad_path = f"{cls.assets_path}/second.h5ad"
 
         first_h5ad.write_h5ad(first_h5ad_path)
         second_h5ad.write_h5ad(second_h5ad_path)
 
-        self.datasets = [
+        cls.datasets = [
             Dataset(dataset_id="first_id", corpora_asset_h5ad_uri="mock", dataset_h5ad_path=first_h5ad_path),
             Dataset(dataset_id="second_id", corpora_asset_h5ad_uri="mock", dataset_h5ad_path=second_h5ad_path),
         ]
@@ -112,11 +112,9 @@ class TestBuilder(unittest.TestCase):
         CENSUS_X_LAYERS_PLATFORM_CONFIG["raw"]["tiledb"]["create"]["dims"]["soma_dim_0"]["tile"] = 2
         CENSUS_X_LAYERS_PLATFORM_CONFIG["raw"]["tiledb"]["create"]["dims"]["soma_dim_1"]["tile"] = 2
 
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        self.td.cleanup()
-        return super().tearDown()
+    @classmethod
+    def teardown_class(cls):
+        cls.td.cleanup()
 
     def test_base_builder_creation(self):
         """
@@ -132,27 +130,30 @@ class TestBuilder(unittest.TestCase):
             from types import SimpleNamespace
 
             args = SimpleNamespace(multi_process=False, consolidate=False, build_tag="test_tag")
-            build(args, self.soma_path, self.assets_path, experiment_builders)
+            return_value = build(args, self.soma_path, self.assets_path, experiment_builders)
+
+            # return_value = 0 means that the validation passed
+            assert return_value == 0
 
             # Query the census and do assertions
             census = cell_census.open_soma(uri=self.soma_path)
 
             # There are 8 cells in total (4 from the first and 4 from the second datasets). They all belong to homo_sapiens
             human_obs = census[CENSUS_DATA_NAME]["homo_sapiens"]["obs"].read().concat().to_pandas()
-            self.assertEqual(human_obs.shape[0], 8)
+            assert human_obs.shape[0] == 8
 
             # mus_musculus should have 0 cells
             mouse_obs = census[CENSUS_DATA_NAME]["mus_musculus"]["obs"].read().concat().to_pandas()
-            self.assertEqual(mouse_obs.shape[0], 0)
+            assert mouse_obs.shape[0] == 0
 
             # There are only 4 unique genes
             var = census[CENSUS_DATA_NAME]["homo_sapiens"]["ms"]["RNA"]["var"].read().concat().to_pandas()
-            self.assertEqual(var.shape[0], 4)
+            assert var.shape[0] == 4
 
             # There should be 2 datasets
             datasets = census[CENSUS_INFO_NAME]["datasets"].read().concat().to_pandas()
-            self.assertEqual(datasets.shape[0], 2)
-            self.assertEqual(list(datasets["dataset_id"]), ["first_id", "second_id"])
+            assert datasets.shape[0] == 2
+            assert list(datasets["dataset_id"]) == ["first_id", "second_id"]
 
     def test_unicode_support(self):
         """
@@ -171,7 +172,3 @@ class TestBuilder(unittest.TestCase):
             pd_df_in = soma.DataFrame(uri=os.path.join(d, "unicode_support")).read().concat().to_pandas()
 
             assert pd_df_in["value"].to_list() == ["Ünicode", "S̈upport"]
-
-
-if __name__ == "__main__":
-    unittest.main()
