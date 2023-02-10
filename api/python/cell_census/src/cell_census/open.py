3e65e45cdb2fbf1a70c095eba5f20c2bf6ba9315
bkmartinjr <bruce@chanzuckerberg.com>
2023-02-10 16:32:47 +0000

    fix newb error in dict copy

--- api/python/cell_census/src/cell_census/open.py
+++ api/python/cell_census/src/cell_census/open.py
@@ -22,9 +22,9 @@ def _open_soma(locator: CensusLocator, context: Optional[soma.options.SOMATileDB
 
     if not context:
         # if no user-defined context, cell_census defaults take precedence over SOMA defaults
-        tiledb_config = DEFAULT_TILEDB_CONFIGURATION
+        tiledb_config = {**DEFAULT_TILEDB_CONFIGURATION}
         if s3_region is not None:
-            tiledb_config.copy()["vfs.s3.region"] = s3_region
+            tiledb_config["vfs.s3.region"] = s3_region
         context = context.replace(tiledb_config=tiledb_config)
     else:
         # if specified, the user context takes precedence _except_ for AWS Region in locator
