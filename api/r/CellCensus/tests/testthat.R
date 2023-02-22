f1172e19c36ecd583803a30641c93996bd8145e3
Mike Lin <dna@mlin.net>
2023-02-21 22:26:34 -1000

    rename cellcensus to CellCensus

--- api/r/CellCensus/tests/testthat.R
+++ api/r/CellCensus/tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(cellcensus)
+library(CellCensus)
 
-test_check("cellcensus")
+test_check("CellCensus")
