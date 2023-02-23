343d9058d6bf385007180251e534314d297ab668
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-02-23 11:26:23 -0500

    Notebook documentation

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = "0.5.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', "nbsphinx"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.rst
+++ docs/index.rst
@@ -26,3 +26,4 @@ Contents
    setup
    python-api
    schema
+   examples
