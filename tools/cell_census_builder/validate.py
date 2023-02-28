c67b9366eecd2df2c49b14a0531edd61d05d95a9
Bruce Martin <bruce@chanzuckerberg.com>
2023-02-27 18:38:44 -0800

    Update python package and builder to tiledbsoma 1.0.0rc2 (#227)
    * checkpoint on changes for rc1 port
    
    * update python package dep to RC2
    
    * fix bug in fragment counts to allow for zero length arrays
    
    * update builder to RC2
    
    * improve validation fix for fragment check on empty arrays
    
    * comments
    
    * PR review feedback
    
    * use functools cache to simplify singleton code

--- tools/cell_census_builder/validate.py
+++ tools/cell_census_builder/validate.py
@@ -447,10 +447,20 @@ def validate_manifest_contents(assets_path: str, datasets: List[Dataset]) -> boo
 
 def validate_consolidation(soma_path: str, experiment_builders: List[ExperimentBuilder]) -> bool:
     """Verify that obs, var and X layers are all fully consolidated & vacuumed"""
+
+    def is_empty_tiledb_array(uri: str) -> bool:
+        with tiledb.open(uri) as A:
+            return A.nonempty_domain() is None
+
     with soma.Collection.open(soma_path, context=SOMA_TileDB_Context()) as census:
         consolidated_uris = list_uris_to_consolidate(census)
         for uri in consolidated_uris:
-            assert len(tiledb.array_fragments(uri)) == 1, f"{uri} has not been fully consolidated & vacuumed"
+            # If an empty array, must have fragment count of zero. If a non-empty array,
+            # must have fragment count of one.
+            assert (len(tiledb.array_fragments(uri)) == 1) or (
+                len(tiledb.array_fragments(uri)) == 0 and is_empty_tiledb_array(uri)
+            ), f"{uri} has not been fully consolidated & vacuumed"
+
     return True
 
 
