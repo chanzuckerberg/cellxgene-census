90971628a13ed0581b76f3077a233f433bf79341
Mike Lin <mlin@contractor.chanzuckerberg.com>
2023-02-28 00:29:11 -1000

    [R] CellCensus package MVP (#206)
    Provides `CellCensus::open_soma(census_version='latest')` to get the top-level `tiledbsoma::SOMACollection`, as well as underlying helper methods for loading the release directory JSON.
    
    - Initial README guidance is to install the package directly from GitHub for now.
    - Initially depends on bleeding-edge builds of tiledbsoma from r-universe.
    - Packaging strategy follows the R Packages book.
    - Includes GH CI workflow and early roxygen docstrings.

---
+++ api/r/CellCensus/R/release_directory.R
@@ -0,0 +1,59 @@
+CELL_CENSUS_RELEASE_DIRECTORY_URL <- "https://s3.us-west-2.amazonaws.com/cellxgene-data-public/cell-census/release.json"
+
+
+#' Get release description for given census version
+#'
+#' @param census_version The census version name.
+#'
+#' @return List with the release location and metadata
+#' @export
+#'
+#' @examples
+get_census_version_description <- function(census_version) {
+  census_directory <- get_census_version_directory()
+  description <- census_directory[census_version, ]
+  if (nrow(description) == 0) {
+    stop(paste("unknown Cell Census version:", census_version))
+  }
+  ans <- as.list(description)
+  ans$census_version <- census_version
+  return(ans)
+}
+
+#' Get the directory of cell census releases currently available
+#'
+#' @return Data frame of available cell census releases, including location and
+#'   metadata.
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+get_census_version_directory <- function() {
+  raw <- jsonlite::fromJSON(CELL_CENSUS_RELEASE_DIRECTORY_URL)
+
+  # Resolve all aliases for easier use
+  for (field in names(raw)) {
+    points_at <- raw[[field]]
+    while (is.character(points_at)) {
+      points_at <- raw[[points_at]]
+    }
+    raw[[field]] <- points_at
+  }
+
+  # Replace NULLs with empty string to facilitate data frame conversion
+  raw <- simple_rapply(raw, function(x) ifelse(is.null(x), "", x))
+
+  # Convert nested list to data frame
+  df <- do.call(rbind, lapply(raw, as.data.frame))
+  rownames(df) <- names(raw)
+  return(df)
+}
+
+# https://stackoverflow.com/a/38950304
+simple_rapply <- function(x, fn) {
+  if (is.list(x)) {
+    lapply(x, simple_rapply, fn)
+  } else {
+    fn(x)
+  }
+}
