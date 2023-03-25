fa95a2bd93cf29cb74b7ffa52fb8c1788dfd2763
Mike Lin <dna@mlin.net>
2023-03-25 13:04:35 -1000

    [r] add get_seurat

--- api/r/CellCensus/tests/testthat/test-get_helpers.R
+++ api/r/CellCensus/tests/testthat/test-get_helpers.R
@@ -31,3 +31,24 @@ test_that("get_presence_matrix", {
     expect_equal(max(pm), 1)
   }
 })
+
+test_that("test_get_seurat", {
+  census <- open_soma()
+  seurat <- get_seurat(
+    census,
+    "Mus musculus",
+    obs_query = tiledbsoma::SOMAAxisQuery$new(value_filter = "tissue_general == 'vasculature'"),
+    obs_column_names = c("soma_joinid", "cell_type", "tissue", "tissue_general", "assay"),
+    var_query = tiledbsoma::SOMAAxisQuery$new(value_filter = "feature_name %in% c('Gm53058', '0610010K14Rik')"),
+    var_column_names = c("soma_joinid", "feature_id", "feature_name", "feature_length")
+  )
+
+  expect_s4_class(seurat, "Seurat")
+  seurat_assay <- seurat@assays[["RNA"]]
+  expect_s4_class(seurat_assay, "Assay")
+  expect_equal(nrow(seurat_assay), 2)
+  expect_gt(ncol(seurat_assay), 0)
+  expect_setequal(seurat_assay[[]][, "feature_name"], c("0610010K14Rik", "Gm53058"))
+  # FIXME: https://github.com/single-cell-data/TileDB-SOMA/issues/1170
+  # expect_equal(sum(seurat[[]][,"tissue_general"] == "vasculature"), ncol(seurat_assay))
+})
