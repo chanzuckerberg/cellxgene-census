6c44df2561aa1bbd4a2f29dd75e0ef376faebe78
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-03-31 13:56:58 -0400

    remove untracked file

--- api/python/cellxgene_census/tests/test_aws.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import cellxgene_census
-import os
-
-def test_can_open_with_anonymous_access():
-    os.environ["AWS_ACCESS_KEY_ID"] = "fake_id"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "fake_key"
-    cellxgene_census.open_soma()
