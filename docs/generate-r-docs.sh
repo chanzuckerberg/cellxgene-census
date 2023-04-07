db80bd82d49fe1f854daa530946b5fe909db293f
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-04-07 10:34:48 -0400

    remove some commented code

--- docs/generate-r-docs.sh
+++ docs/generate-r-docs.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd ../api/r/cellxgene.census
-# Rscript -e 'install.packages("pkgdown")'
-# Rscript -e 'install.packages("usethis")'
-# Rscript -e 'usethis::use_pkgdown()'
 Rscript -e 'pkgdown::build_site()'
 
 mkdir -p ../../../docs/r/
