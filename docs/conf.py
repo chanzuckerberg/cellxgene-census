5876c2e777ea750ff3cbfc213946845840d1fc01
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-05-19 09:12:54 -0400

    autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,16 @@ version = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', "nbsphinx", "sphinx.ext.intersphinx", 'sphinx.ext.napoleon', 'myst_parser']
+extensions = [
+    'sphinx.ext.autodoc', 
+    "nbsphinx", 
+    "sphinx.ext.intersphinx", 
+    'sphinx.ext.napoleon', 
+    'sphinx.ext.autosummary', 
+    'myst_parser'
+    ]
+
+autosummary_generate = True
 
 napoleon_custom_sections = ["Lifecycle"]
 
