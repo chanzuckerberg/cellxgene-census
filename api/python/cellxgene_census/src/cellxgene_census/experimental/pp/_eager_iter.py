60ebd8a7b168f8cf1f7bad6762d56eae9e2b0bbf
bkmartinjr <bruce@chanzuckerberg.com>
2023-05-31 14:04:26 +0000

    add prebuffered iterator

--- api/python/cellxgene_census/src/cellxgene_census/experimental/pp/_eager_iter.py
+++ api/python/cellxgene_census/src/cellxgene_census/experimental/pp/_eager_iter.py
@@ -1,4 +1,6 @@
+from collections import deque
 from concurrent import futures
+import threading
 from typing import Iterator, Optional, TypeVar
 
 _T = TypeVar("_T")
@@ -24,6 +26,58 @@ class EagerIterator(Iterator[_T]):
         except StopIteration:
             self._cleanup()
             raise
+
+    def _cleanup(self) -> None:
+        if self._own_pool:
+            self._pool.shutdown()
+
+    def __del__(self) -> None:
+        # Ensure the threadpool is cleaned up in the case where the
+        # iterator is not exhausted. For more information on __del__:
+        # https://docs.python.org/3/reference/datamodel.html#object.__del__
+        self._cleanup()
+        super_del = getattr(super(), "__del__", lambda: None)
+        super_del()
+
+
+class EagerBufferedIterator(Iterator[_T]):
+    def __init__(
+        self,
+        iterator: Iterator[_T],
+        max_pending: int = 1,
+        pool: Optional[futures.Executor] = None,
+    ):
+        super().__init__()
+        self.iterator = iterator
+        self.max_pending = max_pending
+        self._pool = pool or futures.ThreadPoolExecutor()
+        self._own_pool = pool is None
+        self._pending_results = deque()
+        self._lock = threading.Lock()
+        self._begin_next()
+
+    def __next__(self) -> _T:
+        try:
+            res = self._pending_results[0].result()
+            self._pending_results.popleft()
+            self._begin_next()
+            return res
+        except StopIteration:
+            self._cleanup()
+            raise
+
+    def _begin_next(self):
+        def _fut_done(fut):
+            if fut.exception() is None:
+                self._begin_next()
+
+        with self._lock:
+            not_running = len(self._pending_results) == 0 or self._pending_results[-1].done()
+            if len(self._pending_results) < self.max_pending and not_running:
+                _future = self._pool.submit(self.iterator.__next__)
+                _future.add_done_callback(_fut_done)
+                self._pending_results.append(_future)
+            assert len(self._pending_results) <= self.max_pending
 
     def _cleanup(self) -> None:
         if self._own_pool:
