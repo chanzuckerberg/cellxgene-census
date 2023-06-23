2c8037f9c19c7b2acc4ce35843f0abe0c4f69cec
Pablo E Garcia-Nieto <paedugar@gmail.com>
2023-06-23 10:22:10 -0700

    Add granularity to tests

--- api/python/cellxgene_census/tests/test_acceptance.py
+++ api/python/cellxgene_census/tests/test_acceptance.py
@@ -71,11 +71,7 @@ def table_iter_is_ok(tbl_iter: Iterator[pa.Table], stop_after: Optional[int] = 2
 @pytest.mark.live_corpus
 @pytest.mark.parametrize("organism", ["homo_sapiens", "mus_musculus"])
 @pytest.mark.parametrize("stop_after", [2, pytest.param(None, marks=pytest.mark.expensive)])
-@pytest.mark.parametrize(
-    "ctx_config", 
-    [DEFAULT_TILEDB_CONFIGURATION, pytest.param({"soma.init_buffer_bytes": 4 * 1024**3}, marks=pytest.mark.expensive)]
-)
-def test_incremental_read(organism: str) -> None:
+def test_incremental_read_obs(organism: str, stop_after: Optional[int], ctx_config: Optional[Dict[str, Any]]) -> None:
     """Verify that obs, var and X[raw] can be read incrementally, i.e., in chunks"""
 
     # open census with a small (default) TileDB buffer size, which reduces
@@ -86,10 +82,40 @@ def test_incremental_read(organism: str) -> None:
             census["census_data"][organism].obs.read(column_names=["soma_joinid", "tissue"]),
             stop_after=stop_after
         )
+        
+        
+@pytest.mark.live_corpus
+@pytest.mark.parametrize("organism", ["homo_sapiens", "mus_musculus"])
+@pytest.mark.parametrize("stop_after", [2, pytest.param(None, marks=pytest.mark.expensive)])
+def test_incremental_read_var(organism: str, stop_after: Optional[int], ctx_config: Optional[Dict[str, Any]]) -> None:
+    """Verify that obs, var and X[raw] can be read incrementally, i.e., in chunks"""
+
+    # open census with a small (default) TileDB buffer size, which reduces
+    # memory use, and makes it feasible to run in a GHA.
+    context = make_context("latest")
+    with cellxgene_census.open_soma(census_version="latest", context=context) as census:
         assert table_iter_is_ok(
             census["census_data"][organism].ms["RNA"].var.read(column_names=["soma_joinid", "feature_id"]),
             stop_after=stop_after
         )
+
+
+@pytest.mark.live_corpus
+@pytest.mark.parametrize("organism", ["homo_sapiens", "mus_musculus"])
+@pytest.mark.parametrize(
+    ("stop_after", "ctx_config"),
+    [
+        pytest.param(2, None),
+        pytest.param(None, DEFAULT_TILEDB_CONFIGURATION, marks=pytest.mark.expensive),
+        pytest.param(None, {"soma.init_buffer_bytes": 4 * 1024**3}, marks=pytest.mark.expensive)
+    ]
+)
+def test_incremental_read_X(organism: str, stop_after: Optional[int], ctx_config: Optional[Dict[str, Any]]) -> None:
+    """Verify that obs, var and X[raw] can be read incrementally, i.e., in chunks"""
+
+    ctx_config = ctx_config or {}
+    context = make_context("latest", ctx_config)
+    with cellxgene_census.open_soma(census_version="latest", context=context) as census:
         assert table_iter_is_ok(
             census["census_data"][organism].ms["RNA"].X["raw"].read().tables(), 
             stop_after=stop_after
