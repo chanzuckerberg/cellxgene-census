759d425370ee2466f9712f167d4ceb62ea8c3f14
Mike Lin <mlin@contractor.chanzuckerberg.com>
2023-08-04 00:09:01 -1000

    styler

--- api/r/cellxgene.census/tests/installation/install.R
+++ api/r/cellxgene.census/tests/installation/install.R
@@ -8,15 +8,15 @@ Sys.setenv(MAKE = "make -j8")
 install.packages(
   "tiledb",
   version = "0.20.2",
-  repos = c("https://tiledb-inc.r-universe.dev","https://cloud.r-project.org")
+  repos = c("https://tiledb-inc.r-universe.dev", "https://cloud.r-project.org")
 )
 
 install.packages(
   "cellxgene.census",
-  repos = c("https://chanzuckerberg.r-universe.dev","https://cloud.r-project.org")
+  repos = c("https://chanzuckerberg.r-universe.dev", "https://cloud.r-project.org")
 )
 
 tiledbsoma::show_package_versions()
 print(as.data.frame(
-    cellxgene.census::open_soma()$get("census_info")$get("summary")$read()$concat()
-  ))
+  cellxgene.census::open_soma()$get("census_info")$get("summary")$read()$concat()
+))
