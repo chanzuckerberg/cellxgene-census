chanzuckerberg/cellxgene-census

commit f9f5b5554162ddccfdc2b81c8a68a207b0132bb9

Mirrors support (#552)

tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/__main__.py
@@ -4,7 +4,7 @@ import pathlib
 import sys
 from datetime import datetime
 
-from ..build_state import CENSUS_CONFIG_DEFAULTS, CensusBuildArgs, CensusBuildConfig
+from ..build_state import CensusBuildArgs, CensusBuildConfig
 from ..util import log_process_resource_status, process_init
 from .build_soma import build
 from .validate_soma import validate
@@ -15,8 +15,8 @@ def main() -> int:
     cli_args = cli_parser.parse_args()
     assert cli_args.subcommand in ["build", "validate"]
 
-    config = CensusBuildConfig(**cli_args.__dict__)
-    args = CensusBuildArgs(working_dir=pathlib.PosixPath(cli_args.uri), config=config)
+    default_config = CensusBuildConfig(**cli_args.__dict__)
+    args = CensusBuildArgs(working_dir=pathlib.PosixPath(cli_args.uri), config=default_config)
     process_init(args)
     logging.info(args)
 
@@ -32,6 +32,7 @@ def main() -> int:
 
 
 def create_args_parser() -> argparse.ArgumentParser:
+    default_config = CensusBuildConfig()
     parser = argparse.ArgumentParser(prog="cellxgene_census_builder.build_soma")
     parser.add_argument("uri", type=str, help="Census top-level URI")
     parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase logging verbosity")
@@ -45,7 +46,7 @@ def create_args_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--max_worker_processes",
         type=int,
-        default=CENSUS_CONFIG_DEFAULTS["max_worker_processes"],
+        default=default_config.max_worker_processes,
         help="Limit on number of worker processes",
     )
     parser.add_argument(
@@ -76,7 +77,7 @@ def create_args_parser() -> argparse.ArgumentParser:
     build_parser.add_argument(
         "--dataset_id_blocklist_uri",
         help="Dataset blocklist URI",
-        default=CENSUS_CONFIG_DEFAULTS["dataset_id_blocklist_uri"],
+        default=default_config.dataset_id_blocklist_uri,
     )
     # hidden option for testing by devs. Will process only the first 'n' datasets
     build_parser.add_argument("--test-first-n", type=int)

tools/cellxgene_census_builder/tests/test_manifest.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 import fsspec
 import pytest
 from cellxgene_census_builder.build_soma.manifest import load_manifest
-from cellxgene_census_builder.build_state import CENSUS_CONFIG_DEFAULTS
+from cellxgene_census_builder.build_state import CensusBuildConfig
 
 
 def test_load_manifest_from_file(tmp_path: pathlib.Path, manifest_csv: str, empty_blocklist: str) -> None:
@@ -191,9 +191,11 @@ def test_blocklist_alive_and_well() -> None:
     3. The file "looks like" a block list
     """
 
-    assert CENSUS_CONFIG_DEFAULTS["dataset_id_blocklist_uri"]
+    config = CensusBuildConfig()
 
-    dataset_id_blocklist_uri = CENSUS_CONFIG_DEFAULTS["dataset_id_blocklist_uri"]
+    assert config.dataset_id_blocklist_uri
+
+    dataset_id_blocklist_uri = config.dataset_id_blocklist_uri
 
     # test for existance by reading it. NOTE: if the file moves, this test will fail until
     # the new file location is merged to main.

tools/cellxgene_census_builder/src/cellxgene_census_builder/__main__.py
@@ -1,12 +1,13 @@
 import argparse
 import logging
+import os
 import pathlib
 import sys
-from typing import Callable, List
+from typing import Callable, Sequence
+from urllib.parse import urlparse
 
 import s3fs
 
-from . import __version__
 from .build_state import CENSUS_BUILD_CONFIG, CENSUS_BUILD_STATE, CensusBuildArgs, CensusBuildConfig, CensusBuildState
 from .util import log_process_resource_status, process_init, urlcat
 
@@ -23,7 +24,7 @@ def main() -> int:
     if (working_dir / CENSUS_BUILD_CONFIG).is_file():
         build_config = CensusBuildConfig.load(working_dir / CENSUS_BUILD_CONFIG)
     else:
-        build_config = CensusBuildConfig()
+        build_config = CensusBuildConfig.load_from_env_vars()
 
     if not cli_args.test_resume:
         if (working_dir / CENSUS_BUILD_STATE).exists():
@@ -38,31 +39,69 @@ def main() -> int:
     # Process initialization/setup must be done early. NOTE: do NOT log before this line!
     process_init(build_args)
 
-    # Return process exit code (or raise, which exits with a code of `1`)
-    return do_build(build_args, skip_completed_steps=cli_args.test_resume)
+    # List of available commands for the builder
+    commands = {
+        "release": [do_the_release],
+        "replicate": [do_sync_artifact_to_replica_s3_bucket],
+        "sync-release": [
+            do_sync_release_file_to_replica_s3_bucket,
+        ],
+        "build": [
+            do_prebuild_set_defaults,
+            do_prebuild_checks,
+            do_build_soma,
+            do_validate_soma,
+            do_create_reports,
+            do_data_copy,
+            do_report_copy,
+            do_log_copy,
+        ],
+        "mock-build": [
+            do_mock_build,
+            do_data_copy,
+        ],
+        "cleanup": [
+            do_old_release_cleanup,
+        ],
+        "full-build": [
+            do_prebuild_set_defaults,
+            do_prebuild_checks,
+            do_build_soma,
+            do_validate_soma,
+            do_create_reports,
+            do_data_copy,
+            do_the_release,
+            do_report_copy,
+            do_old_release_cleanup,
+            do_log_copy,
+        ],
+    }
+
+    # The build command is set via the CENSUS_BUILD_COMMAND environment variable.
+    command = os.getenv("CENSUS_BUILD_COMMAND")
+    if command is None:
+        logging.critical("A census command must be specified in the CENSUS_BUILD_COMMAND environment variable.")
+        return 1
+
+    # Used for testing.
+    if command == "pass":
+        return 0
+
+    if command not in commands:
+        logging.critical(f"Unknown command: {command}")
+        return 1
+
+    build_steps = commands[command]
+    rv = _do_steps(build_steps, build_args, cli_args.test_resume)
+    return rv
 
 
-def do_build(args: CensusBuildArgs, skip_completed_steps: bool = False) -> int:
+def _do_steps(
+    build_steps: Sequence[Callable[[CensusBuildArgs], bool]], args: CensusBuildArgs, skip_completed_steps: bool = False
+) -> int:
     """
-    Top-level build sequence.
-
-    Built steps will be executed in order. Build will stop if a build step returns non-zero
-    exit code or raises.
+    Performs a series of steps as specified by the `build_steps` argument.
     """
-    logging.info(f"Census build: start [version={__version__}]")
-    logging.info(args)
-
-    build_steps: List[Callable[[CensusBuildArgs], bool]] = [
-        do_prebuild_set_defaults,
-        do_prebuild_checks,
-        do_build_soma,
-        do_validate_soma,
-        do_create_reports,
-        do_data_copy,
-        do_the_release,
-        do_report_copy,
-        do_old_release_cleanup,
-    ]
     try:
         for n, build_step in enumerate(build_steps, start=1):
             step_n_of = f"Build step {build_step.__name__} [{n} of {len(build_steps)}]"
@@ -78,11 +117,6 @@ def do_build(args: CensusBuildArgs, skip_completed_steps: bool = False) -> int:
             args.state[build_step.__name__] = True
             args.state.commit(args.working_dir / CENSUS_BUILD_STATE)
             logging.info(f"{step_n_of}: complete")
-
-        logging.info("Census build: completed")
-
-        # And last, last, last ... stash the logs
-        do_log_copy(args)
 
     except Exception:
         logging.critical("Caught exception, exiting", exc_info=True)
@@ -153,6 +187,18 @@ def do_create_reports(args: CensusBuildArgs) -> bool:
     return True
 
 
+def do_mock_build(args: CensusBuildArgs) -> bool:
+    """Mock build. Used for testing"""
+    args.soma_path.mkdir(parents=True, exist_ok=False)
+    args.h5ads_path.mkdir(parents=True, exist_ok=False)
+    with open(f"{args.soma_path}/test.soma", "w") as f:
+        f.write("test")
+    with open(f"{args.h5ads_path}/test.h5ad", "w") as f:
+        f.write("test")
+
+    return True
+
+
 def do_data_copy(args: CensusBuildArgs) -> bool:
     """Copy data to S3, in preparation for a release"""
     from .data_copy import sync_to_S3
@@ -166,24 +212,32 @@ def do_data_copy(args: CensusBuildArgs) -> bool:
 
 
 def do_the_release(args: CensusBuildArgs) -> bool:
+    """
+    Perform the release by publishing changes to the release.json file. Respects `dryrun` flag.
+    """
+
     from .release_manifest import CensusVersionDescription, make_a_release
+
+    parsed_url = urlparse(args.config.cellxgene_census_S3_path)
+    prefix = parsed_url.path
 
     release: CensusVersionDescription = {
         "release_date": None,
         "release_build": args.build_tag,
         "soma": {
             "uri": urlcat(args.config.cellxgene_census_S3_path, args.build_tag, "soma/"),
+            "relative_uri": urlcat(prefix, args.build_tag, "soma/"),
             "s3_region": "us-west-2",
         },
         "h5ads": {
             "uri": urlcat(args.config.cellxgene_census_S3_path, args.build_tag, "h5ads/"),
+            "relative_uri": urlcat(prefix, args.build_tag, "h5ads/"),
             "s3_region": "us-west-2",
         },
         "do_not_delete": False,
     }
-    make_a_release(
-        args.config.cellxgene_census_S3_path, args.build_tag, release, make_latest=True, dryrun=args.config.dryrun
-    )
+    census_base_url = args.config.cellxgene_census_S3_path
+    make_a_release(census_base_url, args.build_tag, release, make_latest=True, dryrun=args.config.dryrun)
     return True
 
 
@@ -218,6 +272,35 @@ def do_log_copy(args: CensusBuildArgs) -> bool:
     sync_to_S3(
         args.working_dir / args.config.log_dir,
         urlcat(args.config.logs_S3_path, args.build_tag, args.config.log_dir),
+        dryrun=args.config.dryrun,
+    )
+    return True
+
+
+def do_sync_release_file_to_replica_s3_bucket(args: CensusBuildArgs) -> bool:
+    """Copy release.json to replica S3 bucket"""
+    from .data_copy import sync_to_S3_remote
+
+    source_key = urlcat(args.config.cellxgene_census_S3_path, args.build_tag, "release.json")
+    dest_key = urlcat(args.config.cellxgene_census_S3_replica_path, args.build_tag, "release.json")
+
+    sync_to_S3_remote(
+        source_key,
+        dest_key,
+        dryrun=args.config.dryrun,
+    )
+    return True
+
+
+def do_sync_artifact_to_replica_s3_bucket(args: CensusBuildArgs) -> bool:
+    """
+    Copy data to replica S3 bucket. Only copy the soma artifacts, not the h5ads.
+    """
+    from .data_copy import sync_to_S3_remote
+
+    sync_to_S3_remote(
+        urlcat(args.config.cellxgene_census_S3_path, args.build_tag, "soma/"),
+        urlcat(args.config.cellxgene_census_S3_replica_path, args.build_tag, "soma/"),
         dryrun=args.config.dryrun,
     )
     return True

api/python/cellxgene_census/src/cellxgene_census/__init__.py
@@ -25,7 +25,11 @@ from importlib import metadata
 from ._get_anndata import get_anndata
 from ._open import download_source_h5ad, get_source_h5ad_uri, open_soma
 from ._presence_matrix import get_presence_matrix
-from ._release_directory import get_census_version_description, get_census_version_directory
+from ._release_directory import (
+    get_census_mirror_directory,
+    get_census_version_description,
+    get_census_version_directory,
+)
 
 try:
     __version__ = metadata.version("cellxgene_census")
@@ -38,6 +42,7 @@ __all__ = [
     "get_anndata",
     "get_census_version_description",
     "get_census_version_directory",
+    "get_census_mirror_directory",
     "get_presence_matrix",
     "get_source_h5ad_uri",
     "open_soma",

api/python/cellxgene_census/src/cellxgene_census/_open.py
@@ -9,13 +9,20 @@ Contains methods to open publicly hosted versions of Census object and access it
 import logging
 import os.path
 import urllib.parse
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, get_args
 
 import certifi
 import s3fs
 import tiledbsoma as soma
 
-from ._release_directory import CensusLocator, get_census_version_description
+from ._release_directory import (
+    CensusLocator,
+    CensusMirror,
+    Provider,
+    ResolvedCensusLocator,
+    _get_census_mirrors,
+    get_census_version_description,
+)
 from ._util import _uri_join
 
 DEFAULT_CENSUS_VERSION = "stable"
@@ -33,9 +40,42 @@ api_logger.setLevel(logging.INFO)
 api_logger.addHandler(logging.StreamHandler())
 
 
-def _open_soma(locator: CensusLocator, context: Optional[soma.options.SOMATileDBContext] = None) -> soma.Collection:
-    """Private. Merge config defaults and return open census as a soma Collection/context."""
-    context = _build_soma_tiledb_context(locator.get("s3_region"), context)
+def _assert_mirror_supported(mirror: CensusMirror) -> None:
+    """
+    Verifies if the mirror is supported by this version of the census API.
+    This method provides a proper error message in case an old version of the census
+    tries to connect to an unsupported mirror.
+    """
+    if mirror["provider"] not in get_args(Provider):
+        raise ValueError(
+            f"Unsupported mirror provider: {mirror['provider']}. Try upgrading the cellxgene-census package to the latest version."
+        )
+
+
+def _resolve_census_locator(locator: CensusLocator, mirror: CensusMirror) -> ResolvedCensusLocator:
+    _assert_mirror_supported(mirror)
+
+    if locator.get("relative_uri"):
+        uri = _uri_join(mirror["base_uri"], locator["relative_uri"])
+        region = mirror["region"]
+    else:
+        uri = locator["uri"]
+        region = locator.get("s3_region")
+    return ResolvedCensusLocator(uri=uri, region=region, provider=mirror["provider"])
+
+
+def _open_soma(
+    locator: ResolvedCensusLocator, context: Optional[soma.options.SOMATileDBContext] = None
+) -> soma.Collection:
+    """
+    Private. Merge config defaults and return open census as a soma Collection/context.
+    """
+
+    if locator["provider"] == "S3":
+        context = _build_soma_tiledb_context(locator.get("region"), context)
+    else:  # If no provider is specified, build a default context (don't pass region)
+        context = _build_soma_tiledb_context(None, context)
+
     return soma.open(locator["uri"], mode="r", soma_type=soma.Collection, context=context)
 
 
@@ -68,6 +108,7 @@ def _build_soma_tiledb_context(
 def open_soma(
     *,
     census_version: Optional[str] = DEFAULT_CENSUS_VERSION,
+    mirror: Optional[str] = None,
     uri: Optional[str] = None,
     context: Optional[soma.options.SOMATileDBContext] = None,
 ) -> soma.Collection:
@@ -76,19 +117,23 @@ def open_soma(
     Args:
         census_version:
             The version of the Census, e.g. "latest" or "stable". Defaults to "stable".
+        mirror:
+            The mirror used to retrieve the Census. If not specified, a suitable mirror
+            will be chosen automatically.
         uri:
             The URI containing the Census SOMA objects. If specified, will take precedence
             over ``census_version`` parameter.
         context:
-            A custom :class:`SOMATileDBContext`.
+            A custom :class:`SOMATileDBContext` which will be used to open the SOMA object.
+            Optional, defaults to None.
 
     Returns:
         A SOMA Collection object containing the top-level census.
         It can be used as a context manager, which will automatically close upon exit.
 
     Raises:
-        ValueError: if the census cannot be found, the URI cannot be opened, or neither a URI
-            or a version are specified.
+        ValueError: if the census cannot be found, the URI cannot be opened, neither a URI
+            or a version are specified, or an invalid mirror is provided.
 
     Lifecycle:
         maturing
@@ -120,17 +165,31 @@ def open_soma(
 
         >>> with cellxgene_census.open_soma(uri="/tmp/census") as census:
                 ...
+
+        Open a Census using a mirror.
+
+        >>> with cellxgene_census.open_soma(mirror="s3-us-west-2") as census:
+                ...
     """
 
     if uri is not None:
-        return _open_soma({"uri": uri, "s3_region": None}, context)
+        return _open_soma({"uri": uri, "region": None, "provider": "unknown"}, context)
 
     if census_version is None:
         raise ValueError("Must specify either a census version or an explicit URI.")
 
+    mirrors = _get_census_mirrors()
+    selected_mirror: CensusMirror
+    if mirror is not None:
+        if mirror not in mirrors:
+            raise ValueError("Mirror not found.")
+        selected_mirror = mirrors[mirror]  # type: ignore
+    else:
+        selected_mirror = mirrors[mirrors["default"]]  # type: ignore
+
     try:
         description = get_census_version_description(census_version)  # raises
-    except KeyError:
+    except ValueError:
         # TODO: After the first "stable" is available, this conditional can be removed (keep the 'else' logic)
         if census_version == "stable":
             description = get_census_version_description("latest")
@@ -151,7 +210,9 @@ def open_soma(
             "consistency."
         )
 
-    return _open_soma(description["soma"], context)
+    locator = _resolve_census_locator(description["soma"], selected_mirror)
+
+    return _open_soma(locator, context)
 
 
 def get_source_h5ad_uri(dataset_id: str, *, census_version: str = DEFAULT_CENSUS_VERSION) -> CensusLocator:
@@ -179,7 +240,13 @@ def get_source_h5ad_uri(dataset_id: str, *, census_version: str = DEFAULT_CENSUS
         's3_region': 'us-west-2'}
     """
     description = get_census_version_description(census_version)  # raises
-    census = _open_soma(description["soma"])
+
+    # For h5ads, it makes sense to use the default mirror, since the artifacts themselves won't be mirrored
+    mirrors = _get_census_mirrors()
+    selected_mirror: CensusMirror = mirrors[mirrors["default"]]  # type: ignore
+    census_locator = _resolve_census_locator(description["soma"], selected_mirror)
+
+    census = _open_soma(census_locator)
     dataset = census["census_info"]["datasets"].read(value_filter=f"dataset_id == '{dataset_id}'").concat().to_pandas()
     if len(dataset) == 0:
         raise KeyError("Unknown dataset_id")

commit 86b0bdf26023030b832fef7bdff364fa09bb11a2

[builder][docs] update to CxG schema version 3.1.0 (#740)
* fix regression introduced by PR #552

* migrate from CxG schema 3.0.0 to 3.1.0

* update tests to CxG schema 3.1.0

* additional regression fix for dev UX

* PR review feedback

* update assay filter to match terms used in 3.1 schema

* additional schema copyedit and clarification

tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/__main__.py
@@ -4,6 +4,8 @@ import pathlib
 import sys
 from datetime import datetime
 
+import attrs
+
 from ..build_state import CensusBuildArgs, CensusBuildConfig
 from ..util import log_process_resource_status, process_init
 from .build_soma import build
@@ -15,8 +17,14 @@ def main() -> int:
     cli_args = cli_parser.parse_args()
     assert cli_args.subcommand in ["build", "validate"]
 
-    default_config = CensusBuildConfig(**cli_args.__dict__)
-    args = CensusBuildArgs(working_dir=pathlib.PosixPath(cli_args.uri), config=default_config)
+    # Pass params from CLI arguments _only_ if they exist in the CensusBuildConfig namespace
+    default_config = CensusBuildConfig(
+        **{
+            k: cli_args.__dict__[k]
+            for k in cli_args.__dict__.keys() & {f.alias for f in attrs.fields(CensusBuildConfig)}
+        }
+    )
+    args = CensusBuildArgs(working_dir=pathlib.PosixPath(cli_args.working_dir), config=default_config)
     process_init(args)
     logging.info(args)
 
@@ -34,7 +42,7 @@ def main() -> int:
 def create_args_parser() -> argparse.ArgumentParser:
     default_config = CensusBuildConfig()
     parser = argparse.ArgumentParser(prog="cellxgene_census_builder.build_soma")
-    parser.add_argument("uri", type=str, help="Census top-level URI")
+    parser.add_argument("working_dir", type=str, help="Census build working directory")
     parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase logging verbosity")
     parser.add_argument(
         "-mp",
@@ -66,7 +74,10 @@ def create_args_parser() -> argparse.ArgumentParser:
         help="Manifest file",
     )
     build_parser.add_argument(
-        "--validate", action=argparse.BooleanOptionalAction, default=True, help="Validate immediately after build"
+        "--validate",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Validate immediately after build",
     )
     build_parser.add_argument(
         "--consolidate",
@@ -80,9 +91,9 @@ def create_args_parser() -> argparse.ArgumentParser:
         default=default_config.dataset_id_blocklist_uri,
     )
     # hidden option for testing by devs. Will process only the first 'n' datasets
-    build_parser.add_argument("--test-first-n", type=int)
+    build_parser.add_argument("--test-first-n", type=int, default=0)
     # hidden option for testing by devs. Allow for WIP testing by devs.
-    build_parser.add_argument("--test-disable-dirty-git-check", action=argparse.BooleanOptionalAction)
+    build_parser.add_argument("--disable-dirty-git-check", action=argparse.BooleanOptionalAction, default=False)
 
     # VALIDATE
     subparsers.add_parser("validate", help="Validate an existing Census build")

tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/anndata.py
@@ -8,7 +8,7 @@ import scipy.sparse as sparse
 
 from ..util import urlcat
 from .datasets import Dataset
-from .globals import CXG_SCHEMA_VERSION, CXG_SCHEMA_VERSION_IMPORT, FEATURE_REFERENCE_IGNORE
+from .globals import CXG_SCHEMA_VERSION, FEATURE_REFERENCE_IGNORE
 
 AnnDataFilterSpec = TypedDict(
     "AnnDataFilterSpec",
@@ -39,10 +39,14 @@ def open_anndata(
         logging.debug(f"open_anndata: {path}")
         ad = anndata.read_h5ad(path, *args, **kwargs)
 
-        assert CXG_SCHEMA_VERSION == "3.0.0"
+        # These are schema versions this code is known to work with. This is a
+        # sanity check, which would be better implemented via a unit test at
+        # some point in the future.
+        assert CXG_SCHEMA_VERSION in ["3.1.0", "3.0.0"]
+
         if h5ad.schema_version == "":
             h5ad.schema_version = get_cellxgene_schema_version(ad)
-        if h5ad.schema_version not in CXG_SCHEMA_VERSION_IMPORT:
+        if h5ad.schema_version != CXG_SCHEMA_VERSION:
             msg = f"H5AD {h5ad.dataset_h5ad_path} has unsupported schema version {h5ad.schema_version}, expected {CXG_SCHEMA_VERSION}"
             logging.error(msg)
             raise RuntimeError(msg)

tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/globals.py
@@ -5,13 +5,13 @@ import pyarrow as pa
 import tiledb
 import tiledbsoma as soma
 
-CENSUS_SCHEMA_VERSION = "1.1.0"
+CENSUS_SCHEMA_VERSION = "1.2.0"
 
-CXG_SCHEMA_VERSION = "3.0.0"  # version we write to the census
+CXG_SCHEMA_VERSION = "3.1.0"  # the CELLxGENE schema version supported
+
 # NOTE: The UBERON ontology URL needs to manually updated if the CXG Dataset Schema is updated. This is a temporary
 # hassle, however, since the TissueMapper, which relies upon this ontology, will eventually be removed from the Builder
-CXG_UBERON_ONTOLOGY_URL = "https://github.com/obophenotype/uberon/releases/download/v2022-08-19/uberon.owl"
-CXG_SCHEMA_VERSION_IMPORT = [CXG_SCHEMA_VERSION]  # versions we can ingest
+CXG_UBERON_ONTOLOGY_URL = "https://github.com/obophenotype/uberon/releases/download/v2023-06-28/uberon.owl"
 
 # Columns expected in the census_datasets dataframe
 CENSUS_DATASETS_COLUMNS = [
@@ -293,23 +293,23 @@ CENSUS_X_LAYERS_PLATFORM_CONFIG = {
     },
 }
 
-# list of EFO terms that correspond to RNA seq modality/measurement
+# list of EFO terms that correspond to RNA seq modality/measurement. These terms
+# define the inclusive filter applied to obs.assay_ontology_term_id. All other
+# terms are excluded from the Census.
 RNA_SEQ = [
     "EFO:0008720",  # DroNc-seq
     "EFO:0008722",  # Drop-seq
     "EFO:0008780",  # inDrop
-    "EFO:0008913",  # single-cell RNA sequencing
+    "EFO:0008796",  # MARS-seq
     "EFO:0008919",  # Seq-Well
     "EFO:0008930",  # Smart-seq
     "EFO:0008931",  # Smart-seq2
     "EFO:0008953",  # STRT-seq
-    "EFO:0008995",  # 10x technology
     "EFO:0009899",  # 10x 3' v2
     "EFO:0009900",  # 10x 5' v2
     "EFO:0009901",  # 10x 3' v1
     "EFO:0009922",  # 10x 3' v3
     "EFO:0010010",  # CEL-seq2
-    "EFO:0010183",  # single cell library construction
     "EFO:0010550",  # sci-RNA-seq
     "EFO:0011025",  # 10x 5' v1
     "EFO:0030002",  # microwell-seq
@@ -318,6 +318,9 @@ RNA_SEQ = [
     "EFO:0030019",  # Seq-Well S3
     "EFO:0700003",  # BD Rhapsody Whole Transcriptome Analysis
     "EFO:0700004",  # BD Rhapsody Targeted mRNA
+    "EFO:0700010",  # TruDrop
+    "EFO:0700011",  # GEXSCOPE technology
+    "EFO:0700016",  # Smart-seq v4
 ]
 
 DONOR_ID_IGNORE = ["pooled", "unknown"]

tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/manifest.py
@@ -7,7 +7,7 @@ from typing import List, Set
 import fsspec
 
 from .datasets import Dataset
-from .globals import CXG_SCHEMA_VERSION, CXG_SCHEMA_VERSION_IMPORT
+from .globals import CXG_SCHEMA_VERSION
 from .util import fetch_json
 
 CXG_BASE_URI = "https://api.cellxgene.cziscience.com/"
@@ -63,7 +63,7 @@ def load_manifest_from_CxG() -> List[Dataset]:
         dataset_id = dataset["dataset_id"]
         schema_version = dataset["schema_version"]
 
-        if schema_version not in CXG_SCHEMA_VERSION_IMPORT:
+        if schema_version != CXG_SCHEMA_VERSION:
             msg = f"Manifest fetch: dataset {dataset_id} contains unsupported schema version {schema_version}."
             logging.error(msg)
             raise RuntimeError(msg)

tools/cellxgene_census_builder/tests/anndata/test_anndata.py
@@ -125,4 +125,4 @@ def test_make_anndata_cell_filter_assay(h5ad_with_assays: ad.AnnData) -> None:
 
 def test_get_cellxgene_schema_version(h5ad_simple: ad.AnnData) -> None:
     version = get_cellxgene_schema_version(h5ad_simple)
-    assert version == "3.0.0"
+    assert version == "3.1.0"

tools/cellxgene_census_builder/tests/conftest.py
@@ -95,7 +95,7 @@ def get_h5ad(organism: Organism, gene_ids: Optional[List[str]] = None, no_zero_c
     uns["batch_condition"] = np.array(["a", "b"], dtype="object")
 
     # Need to carefully set the corpora schema versions in order for tests to pass.
-    uns["schema_version"] = "3.0.0"  # type: ignore
+    uns["schema_version"] = "3.1.0"  # type: ignore
 
     return anndata.AnnData(X=X, obs=obs, var=var, obsm=obsm, uns=uns)
 

tools/cellxgene_census_builder/tests/test_main.py
@@ -7,7 +7,7 @@ def test_create_args_parser_default_build() -> None:
 
     args = parser.parse_args(args=args_list)
 
-    assert args.uri == "."
+    assert args.working_dir == "."
     assert args.verbose == 0
     assert args.multi_process is False
     assert isinstance(args.build_tag, str)
@@ -22,7 +22,7 @@ def test_create_args_parser_default_validate() -> None:
 
     args = parser.parse_args(args=args_list)
 
-    assert args.uri == "."
+    assert args.working_dir == "."
     assert args.verbose == 0
     assert args.multi_process is False
     assert isinstance(args.build_tag, str)

tools/cellxgene_census_builder/tests/test_manifest.py
@@ -68,7 +68,7 @@ def test_load_manifest_from_cxg(empty_blocklist: str) -> None:
                 "collection_name": "1",
                 "collection_doi": None,
                 "title": "dataset #1",
-                "schema_version": "3.0.0",
+                "schema_version": "3.1.0",
                 "assets": [
                     {
                         "filesize": 123,
@@ -89,7 +89,7 @@ def test_load_manifest_from_cxg(empty_blocklist: str) -> None:
                 "collection_name": "1",
                 "collection_doi": None,
                 "title": "dataset #2",
-                "schema_version": "3.0.0",
+                "schema_version": "3.1.0",
                 "assets": [{"filesize": 456, "filetype": "H5AD", "url": "https://fake.url/dataset_id_2.h5ad"}],
                 "dataset_version_id": "dataset_id_2",
             },
@@ -119,7 +119,7 @@ def test_load_manifest_from_cxg_errors_on_datasets_with_old_schema(
                 "collection_name": "1",
                 "collection_doi": None,
                 "title": "dataset #1",
-                "schema_version": "3.0.0",
+                "schema_version": "3.1.0",
                 "assets": [{"filesize": 123, "filetype": "H5AD", "url": "https://fake.url/dataset_id_1.h5ad"}],
                 "dataset_version_id": "dataset_id_1",
             },
@@ -158,7 +158,7 @@ def test_load_manifest_from_cxg_excludes_datasets_with_no_assets(
                 "collection_name": "1",
                 "collection_doi": None,
                 "title": "dataset #1",
-                "schema_version": "3.0.0",
+                "schema_version": "3.1.0",
                 "assets": [{"filesize": 123, "filetype": "H5AD", "url": "https://fake.url/dataset_id_1.h5ad"}],
                 "dataset_version_id": "dataset_id_1",
             },
@@ -168,7 +168,7 @@ def test_load_manifest_from_cxg_excludes_datasets_with_no_assets(
                 "collection_name": "1",
                 "collection_doi": None,
                 "title": "dataset #2",
-                "schema_version": "3.0.0",
+                "schema_version": "3.1.0",
                 "assets": [],
                 "dataset_version_id": "dataset_id_2",
             },