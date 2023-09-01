7c6bbc55642a649f550b1742dcc7c2cc9b14e118
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-09-01 11:49:38 -0400

    log only tail of dmesg output

--- tools/cellxgene_census_builder/entrypoint.sh
+++ tools/cellxgene_census_builder/entrypoint.sh
@@ -2,7 +2,7 @@
 
 python3 -m cellxgene_census_builder .
 BUILDER_STATUS=$?
-# On error, log dmesg to aid troubleshooting
+# On error, log dmesg tail to aid troubleshooting
 # Note: requires docker --privileged option
-if [[ $BUILDER_STATUS -ne 0 ]]; then dmesg; fi
+if [[ $BUILDER_STATUS -ne 0 ]]; then dmesg | tail -n 128; fi
 exit $BUILDER_STATUS
