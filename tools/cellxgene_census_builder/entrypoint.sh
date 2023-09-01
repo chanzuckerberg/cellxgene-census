03edbbee19c58a778822ae17560b7f5b7fffff13
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-09-01 10:21:46 -0400

    add builder dmesg logging on error

--- tools/cellxgene_census_builder/entrypoint.sh
+++ tools/cellxgene_census_builder/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 python3 -m cellxgene_census_builder .
+BUILDER_STATUS=$?
+# On error, log dmesg to aid troubleshooting
+# Note: requires docker --privileged option
+if [[ $BUILDER_STATUS -ne 0 ]]; then dmesg; fi
+exit $BUILDER_STATUS
