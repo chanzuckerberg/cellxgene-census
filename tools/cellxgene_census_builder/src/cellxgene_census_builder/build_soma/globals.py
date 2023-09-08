3bcc4328e914bf0a68a23cd21ca1565833b58270
bkmartinjr <bruce@chanzuckerberg.com>
2023-09-08 13:58:57 +0000

    thread usage tuning

--- tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/globals.py
+++ tools/cellxgene_census_builder/src/cellxgene_census_builder/build_soma/globals.py
@@ -5,6 +5,8 @@ import pyarrow as pa
 import tiledb
 import tiledbsoma as soma
 
+from ..util import cpu_count
+
 CENSUS_SCHEMA_VERSION = "1.2.0"
 
 CXG_SCHEMA_VERSION = "3.1.0"  # the CELLxGENE schema version supported
@@ -336,6 +338,13 @@ DEFAULT_TILEDB_CONFIG = {
     "py.deduplicate": "true",
     "soma.init_buffer_bytes": 1 * 1024**3,
     "sm.consolidation.buffer_size": 3 * 1024**3,
+    "sm.mem.reader.sparse_global_order.ratio_array_data": 0.3,
+    #
+    # Concurrency levels are capped for high-CPU boxes. Left unchecked, some
+    # of the largest host configs can bump into Linux kernel thread limits,
+    # without any real benefit to overall performance.
+    "sm.compute_concurrency_level": min(cpu_count(), 64),
+    "sm.io_concurrency_level": min(cpu_count(), 64),
 }
 
 
--- tools/cellxgene_census_builder/src/cellxgene_census_builder/util.py
+++ tools/cellxgene_census_builder/src/cellxgene_census_builder/util.py
@@ -41,12 +41,25 @@ def urlcat(base: str, *paths: str) -> str:
     return url
 
 
-def env_var_init(args: CensusBuildArgs) -> None:
+def env_var_init() -> None:
     """
     Set environment variables as needed by dependencies, etc.
+
+    This controls thread allocation for worker (child) processes. It is executed too
+    late to influence __init__ time thread pool allocations for the main process.
     """
     if "NUMEXPR_MAX_THREADS" not in os.environ:
-        os.environ["NUMEXPR_MAX_THREADS"] = str(min(1, cpu_count() // 2))
+        # ref: https://numexpr.readthedocs.io/en/latest/user_guide.html#threadpool-configuration
+        # In particular, the docs state that >8 threads is not helpful except in extreme circumstances.
+        os.environ["NUMEXPR_MAX_THREADS"] = str(min(8, max(1, cpu_count() // 2)))
+
+    # Each of these control thread-pool allocation for commonly used packages that
+    # may be pulled into our environment, and which have import-time pool allocation.
+    # Where we are confident we have no performance dependency related to their concurrency,
+    # set their pool size to "1".
+    for env_name in ["OMP_NUM_THREADS", "OPENBLAS_NUM_THREADS", "MKL_NUM_THREADS", "VECLIB_MAXIMUM_THREADS"]:
+        if env_name not in os.environ:
+            os.environ[env_name] = "1"
 
 
 def process_init(args: CensusBuildArgs) -> None:
@@ -58,7 +71,7 @@ def process_init(args: CensusBuildArgs) -> None:
     if multiprocessing.get_start_method(True) != "spawn":
         multiprocessing.set_start_method("spawn", True)
 
-    env_var_init(args)
+    env_var_init()
 
     # these are super noisy!
     numba_logger = logging.getLogger("numba")
