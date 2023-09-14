9f90fa3476dba8a3e7d6fb611ed6401d0f9e0d77
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-09-14 14:25:32 -0400

    [builder] Add additional debug logging (#770)
    * [builder] Add additional debug logging
    
    * Move to entrypoint
    
    * Revert main file

--- tools/cellxgene_census_builder/entrypoint.sh
+++ tools/cellxgene_census_builder/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Log tiledbsoma package versions
+python -c 'import tiledbsoma; print(tiledbsoma.show_package_versions())'
+
+# Log pip freeze 
+pip freeze
+
 python3 -m cellxgene_census_builder .
 BUILDER_STATUS=$?
 # On error, log dmesg tail to aid troubleshooting
 # Note: requires docker --privileged option
 if [[ $BUILDER_STATUS -ne 0 ]]; then dmesg | tail -n 128; fi
+if [[ $BUILDER_STATUS -ne 0 ]]; then df; fi
 exit $BUILDER_STATUS
