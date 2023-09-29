76f29735c13ec36da1a10d1ecc8cb3d1a4632082
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-09-29 15:13:49 -0700

    fix typo

--- tools/scripts/scvi/scvi-train.py
+++ tools/scripts/scvi/scvi-train.py
@@ -27,7 +27,7 @@ min_genes = hvg_config["min_genes"]
 hvgs_df = highly_variable_genes(query, n_top_genes=top_n_hvg, batch_key=hvg_batch)
 
 hv = hvgs_df.highly_variable
-hv_idx = hv[hv is True].index
+hv_idx = hv[hv == True].index # type: ignore
 
 query = census["census_data"][experiment_name].axis_query(
     measurement_name="RNA",
