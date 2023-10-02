be0258a33a2cff6e7e0dc53989d55a92967c774c
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-10-02 08:33:52 -0700

    more changes

--- tools/scripts/scvi/scvi-train.py
+++ tools/scripts/scvi/scvi-train.py
@@ -11,20 +11,20 @@ file = "scvi-config.yaml"
 with open(file) as f:
     config = yaml.safe_load(f)
 
-census = cellxgene_census.open_soma()
+census = cellxgene_census.open_soma(census_version="latest")
 
-census_config = config["census"]
-experiment_name = census_config["organism"]
-obs_value_filter = census_config["obs_query"]
+census_config = config.get("census")
+experiment_name = census_config.get("organism")
+obs_value_filter = census_config.get("obs_query")
 
 query = census["census_data"][experiment_name].axis_query(
     measurement_name="RNA", obs_query=soma.AxisQuery(value_filter=obs_value_filter)
 )
 
-hvg_config = config["hvg"]
-top_n_hvg = hvg_config["top_n_hvg"]
-hvg_batch = hvg_config["hvg_batch"]
-min_genes = hvg_config["min_genes"]
+hvg_config = config.get("hvg")
+top_n_hvg = hvg_config.get("top_n_hvg")
+hvg_batch = hvg_config.get("hvg_batch")
+min_genes = hvg_config.get("min_genes")
 
 hvgs_df = highly_variable_genes(query, n_top_genes=top_n_hvg, batch_key=hvg_batch)
 
@@ -46,26 +46,26 @@ print("Last run with scvi-tools version:", scvi.__version__)
 
 scvi.model.SCVI.setup_anndata(ad, batch_key="batch")
 
-model_config = config["model"]
-n_hidden = model_config["n_hidden"]
-n_latent = model_config["n_latent"]
-n_layers = model_config["n_layers"]
-dropout_rate = model_config["dropout_rate"]
+model_config = config.get("model")
+n_hidden = model_config.get("n_hidden")
+n_latent = model_config.get("n_latent")
+n_layers = model_config.get("n_layers")
+dropout_rate = model_config.get("dropout_rate")
 
 model = scvi.model.SCVI(ad, n_layers=n_layers, n_latent=n_latent, gene_likelihood="nb")
 
-train_config = config["train"]
-max_epochs = train_config["max_epochs"]
-batch_size = train_config["batch_size"]
-train_size = train_config["train_size"]
-early_stopping = train_config["early_stopping"]
-devices = train_config["devices"]
+train_config = config.get("train")
+max_epochs = train_config.get("max_epochs")
+batch_size = train_config.get("batch_size")
+train_size = train_config.get("train_size")
+early_stopping = train_config.get("early_stopping")
+devices = train_config.get("devices")
 
-trainer_config = train_config["trainer"]
+trainer_config = train_config.get("trainer")
 
-training_plan_config = config["training_plan"]
+training_plan_config = config.get("training_plan")
 
-scvi.settings.dl_num_workers = train_config["num_workers"]
+scvi.settings.dl_num_workers = train_config.get("num_workers")
 
 model.train(
     max_epochs=max_epochs,
@@ -74,7 +74,7 @@ model.train(
     early_stopping=early_stopping,
     plan_kwargs=training_plan_config,
     trainer_kwargs=trainer_config,
-    strategy="ddp_find_unused_parameters_true",
+    strategy="ddp_find_unused_parameters_true", # Required for Multi-GPU training.
     devices=devices,
 )
 
