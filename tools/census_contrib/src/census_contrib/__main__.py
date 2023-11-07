9aa44447e7f6c69f22682d2bac3bd6905c577dda
bkmartinjr <bruce@chanzuckerberg.com>
2023-11-07 23:16:09 +0000

    add logging

--- tools/census_contrib/src/census_contrib/__main__.py
+++ tools/census_contrib/src/census_contrib/__main__.py
@@ -4,6 +4,7 @@ import logging
 import sys
 
 from .args import Arguments
+from .embedding import validate_embedding
 from .metadata import load_metadata, validate_metadata
 from .save import save_as_soma
 
@@ -14,10 +15,12 @@ def main() -> int:
 
     args.logger.info("Starting")
     metadata = load_metadata(args)
-    args.logger.info("Validating metadata")
+    args.logger.info("Validate metadata")
     validate_metadata(args, metadata)
     args.logger.info("Loading embedding")
     embedding = args.ingestor(args, metadata)
+    args.logger.info(f"Validate embedding [shape={embedding.shape}]")
+    validate_embedding(args, metadata, embedding)
     args.logger.info("Saving")
     save_as_soma(args, metadata, embedding)
     args.logger.info("Finished")
@@ -35,6 +38,10 @@ def setup_logging(args: Arguments) -> None:
 
     logger = logging.getLogger("census_contrib")
     args.logger = logger
+
+    # turn down some other stuff
+    logging.getLogger('numba').setLevel(logging.WARNING)
+
 
 
 if __name__ == "__main__":
