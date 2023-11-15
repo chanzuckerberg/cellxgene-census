490e4dfc4bda537ab1cf984f201d73f45baa9660
Mike Lin <mlin@contractor.chanzuckerberg.com>
2023-11-15 10:51:13 -0800

    revise geneformer tests

--- api/python/cellxgene_census/src/cellxgene_census/experimental/ml/huggingface/geneformer_tokenizer.py
+++ api/python/cellxgene_census/src/cellxgene_census/experimental/ml/huggingface/geneformer_tokenizer.py
@@ -15,7 +15,7 @@ class GeneformerTokenizer(CellDatasetBuilder):
     cell in CELLxGENE Census ExperimentAxisQuery results (human).
 
     This class requires the Geneformer package to be installed separately with:
-    `pip install git+https://huggingface.co/ctheodoris/Geneformer@39ab62e`
+    `pip install git+https://huggingface.co/ctheodoris/Geneformer@8df5dc1`
 
     Example usage:
 
@@ -107,7 +107,7 @@ class GeneformerTokenizer(CellDatasetBuilder):
                 # pyproject.toml can't express Geneformer git+https dependency
                 raise ImportError(
                     "Please install Geneformer with: "
-                    "pip install git+https://huggingface.co/ctheodoris/Geneformer@39ab62e"
+                    "pip install git+https://huggingface.co/ctheodoris/Geneformer@8df5dc1"
                 ) from None
             if not token_dictionary_file:
                 token_dictionary_file = geneformer.tokenizer.TOKEN_DICTIONARY_FILE
