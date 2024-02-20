eb62755bb817172db2f63bb5d3b579710da925fb
Bruce Martin <bruce@chanzuckerberg.com>
2024-02-20 10:06:01 -0800

    thread usage config (#1006)

--- tools/cellxgene_census_builder/entrypoint.sh
+++ tools/cellxgene_census_builder/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# These configure thread allocation for BLAS, OpenMP, etc. We don't use these
+# packages, but they will allocate a large number of threads at startup (usually
+# one thread per host CPU). Turn down the allocation which gets excessive in
+# high-CPU hosts, such as those used to build the Census.
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+
 # Log tiledbsoma package versions
 python3 -c 'import tiledbsoma; tiledbsoma.show_package_versions()'
 
