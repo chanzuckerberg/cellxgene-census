430c9d6e060fab588d97fc2dd274cec931f7c902
Martin Kim <46072231+martinkim0@users.noreply.github.com>
2024-02-21 10:22:30 -0800

    Update api/python/cellxgene_census/src/cellxgene_census/experimental/__init__.py
    Co-authored-by: Emanuele Bezzi <bezzi.emanuele@gmail.com>

--- api/python/cellxgene_census/src/cellxgene_census/experimental/__init__.py
+++ api/python/cellxgene_census/src/cellxgene_census/experimental/__init__.py
@@ -1,4 +1,4 @@
-"""Experimental API for the CELLxGENE Disover Census."""
+"""Experimental API for the CELLxGENE Discover Census."""
 
 from ._embedding import get_embedding, get_embedding_metadata
 
