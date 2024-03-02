9af025451e78549af9ef7d8e91d027e2854034ce
bkmartinjr <bruce@chanzuckerberg.com>
2024-03-02 17:18:29 +0000

    fix builder test

--- tools/cellxgene_census_builder/tests/test_builder.py
+++ tools/cellxgene_census_builder/tests/test_builder.py
@@ -27,7 +27,7 @@ from cellxgene_census_builder.build_state import CensusBuildArgs
 
 @pytest.mark.parametrize(
     "census_build_args",
-    [{"multi_process": False, "consolidate": True, "build_tag": "test_tag", "verbose": 0}],
+    [{"multi_process": False, "consolidate": False, "build_tag": "test_tag", "verbose": 0}],
     indirect=True,
 )
 def test_base_builder_creation(
@@ -38,12 +38,13 @@ def test_base_builder_creation(
     """
     Runs the builder, queries the census and performs a set of base assertions.
     """
-    with patch("cellxgene_census_builder.build_soma.build_soma.prepare_file_system"), patch(
-        "cellxgene_census_builder.build_soma.build_soma.build_step1_get_source_datasets", return_value=datasets
-    ), patch("cellxgene_census_builder.build_soma.build_soma.consolidate"), patch(
-        "cellxgene_census_builder.build_soma.build_soma.go_validate", return_value=True
-    ), patch(
-        "cellxgene_census_builder.build_soma.build_soma.start_async_consolidation",
+    with (
+        patch("cellxgene_census_builder.build_soma.build_soma.prepare_file_system"),
+        patch("cellxgene_census_builder.build_soma.build_soma.build_step1_get_source_datasets", return_value=datasets),
+        patch("cellxgene_census_builder.build_soma.build_soma.validate_consolidation", return_value=True),
+        patch(
+            "cellxgene_census_builder.build_soma.build_soma.start_async_consolidation",
+        ),
     ):
         return_value = build(census_build_args)
 
