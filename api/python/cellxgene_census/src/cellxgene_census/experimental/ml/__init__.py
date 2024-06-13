f688020f6d2c152b354a39c985be3287adbfc341
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2024-06-13 15:09:43 -0700

    Revert some changes

--- api/python/cellxgene_census/src/cellxgene_census/experimental/ml/__init__.py
+++ api/python/cellxgene_census/src/cellxgene_census/experimental/ml/__init__.py
@@ -1,12 +1,10 @@
 """An API to facilitate use of PyTorch ML training with data from the CZI Science CELLxGENE Census."""
 
-from .datamodule import CensusSCVIDataModule
 from .pytorch import Encoder, ExperimentDataPipe, Stats, experiment_dataloader
 
 __all__ = [
     "Stats",
     "ExperimentDataPipe",
     "experiment_dataloader",
-    "CensusSCVIDataModule",
     "Encoder",
 ]
