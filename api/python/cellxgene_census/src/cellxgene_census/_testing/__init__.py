5c14af81c5ccd01e5f5b300f596512ee547cdd77
Isaac Virshup <ivirshup@gmail.com>
2024-06-20 17:36:25 +0000

    Remove some cruft from the proxy plugin

--- api/python/cellxgene_census/src/cellxgene_census/_testing/__init__.py
+++ api/python/cellxgene_census/src/cellxgene_census/_testing/__init__.py
@@ -17,21 +17,17 @@ flags.add_argument(
 )
 
 
-class ProxyPlugin(proxy.http.proxy.HttpProxyBasePlugin):
+class ProxyPlugin(proxy.http.proxy.HttpProxyBasePlugin):  # type: ignore
     def handle_client_request(self, request: proxy.http.parser.HttpParser) -> proxy.http.parser.HttpParser:
         # If anything fails in here, it just fails to respond
-        # return request
-        # print(request.headers,)
         try:
             with Path(self.flags.request_logfile).open("a") as f:
                 record = {
                     "method": request.method.decode(),
                     "url": str(request._url),
-                    # "headers": {k2.decode().lower(): v.decode() for _, (k2, v) in request.headers.items()} if request.header else {},
                 }
 
                 if request.headers:
-                    # record["headers"] = {k.decode(): [v.decode() for v in vs] for k, vs in request.headers.items()}
                     record["headers"] = {k2.decode().lower(): v.decode() for _, (k2, v) in request.headers.items()}
                 print("Request:")
                 print(record)
@@ -43,13 +39,3 @@ class ProxyPlugin(proxy.http.proxy.HttpProxyBasePlugin):
             traceback.print_exception(e)
             raise e
         return request
-
-    # def handle_upstream_chunk(self, chunk):
-    #     print("Response:")
-    #     as_bytes = bytes(chunk)
-    #     if len(as_bytes) > 1000:
-    #         print(as_bytes[:300])
-    #     else:
-    #         print(as_bytes)
-    #     print()
-    #     return chunk
